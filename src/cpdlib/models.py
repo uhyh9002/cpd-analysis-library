1cb31cb495dc9d69efb5bead5d0f649a684b16a0
uhyh9002 <52909201+uhyh9002@users.noreply.github.com>
2025-07-08 10:14:30 +0900

    Update models.py

--- src/cpdlib/models.py
+++ src/cpdlib/models.py
@@ -1,3 +1,6 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
 # ==============================================================================
 # 섹션 2: 변화점 탐지(PELT) 모델링 함수
